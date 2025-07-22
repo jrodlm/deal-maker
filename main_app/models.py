6e37753fe03c5d5615f3299d323da9179c3e3763
jrodlm <jarodlmorris@gmail.com>
2025-07-22 11:42:54 -0600

    more progress on views

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Deal(models.Model):
+    company_name = models.CharField(max_length=100)
+    industry = models.CharField(max_length=100)
+    asking_price = models.IntegerField()
+    revenue = models.IntegerField()
+    sde = models.IntegerField()
+    status = models.CharField(max_length=100)
+    notes = models.CharField(max_length=100)
+    created_on = models.DateField()
+
+    def __str__(self):
+        return self.company_name
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Hello, Lets Do a Deal!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
