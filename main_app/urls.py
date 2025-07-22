3536ee08cd0004abfbd9b5d405777bb9c2a02cbd
jrodlm <jarodlmorris@gmail.com>
2025-07-22 17:59:48 -0600

    updated views - dynamic database created, admin created

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('deals/', views.deal_index, name='deal_index'),
+    path('deals/<int:deal_id>/', views.deal_detail, name='deal-detail'),
+
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Deal
 
 
 def home(request):
@@ -9,23 +9,27 @@ def about(request):
     return render(request, 'about.html')
 
 def deal_index(request):
+    deals = Deal.objects.all()
     return render(request, 'deals/index.html', {'deals': deals})
 
+def deal_detail(request, cat_id):
+    deal = Deal.objects.get(id=deal_id)
+    return render(request, 'deals/detail.html', {'deal': deal})
 
-class Deal:
-    def __init__(self, company_name, industry, asking_price, revenue, sde, status, notes, created_on):
-        self.company_name = company_name
-        self.industry = industry
-        self.asking_price = asking_price
-        self.revenue = revenue
-        self.sde = sde
-        self.status = status
-        self.notes = notes
-        self.created_on = created_on
+# class Deal:
+#     def __init__(self, company_name, industry, asking_price, revenue, sde, status, notes, created_on):
+#         self.company_name = company_name
+#         self.industry = industry
+#         self.asking_price = asking_price
+#         self.revenue = revenue
+#         self.sde = sde
+#         self.status = status
+#         self.notes = notes
+#         self.created_on = created_on
 
-deals = [
-    Deal('Acme', 'B2B', '$1,420,000', '$725,000', '$250,000',  'new', 'n/a','TBD'),
-    Deal('AI Hub', 'SaaS', '$2,212,000', '$1,233,000', '$350,000', 'expressed interest', 'n/a', 'TBD'),
-    Deal('Service Masters', 'Industrial Services', '$1,489,000', '$999,000','$450,000','under loi', 'n/a','TBD'),
-    Deal('Plug It', 'Plumbing', '$1,745,000','$1,410,000', '$550,000','expressed interest','n/a','TBD')
-]
+# deals = [
+#     Deal('Acme', 'B2B', '$1,420,000', '$725,000', '$250,000',  'new', 'n/a','TBD'),
+#     Deal('AI Hub', 'SaaS', '$2,212,000', '$1,233,000', '$350,000', 'expressed interest', 'n/a', 'TBD'),
+#     Deal('Service Masters', 'Industrial Services', '$1,489,000', '$999,000','$450,000','under loi', 'n/a','TBD'),
+#     Deal('Plug It', 'Plumbing', '$1,745,000','$1,410,000', '$550,000','expressed interest','n/a','TBD')
+# ]
